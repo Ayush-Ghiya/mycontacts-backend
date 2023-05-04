e820aee34044f32c169456ee53b1b8ec7b046bfe
Ayush-Ghiya <97225224+Ayush-Ghiya@users.noreply.github.com>
2023-05-04 16:48:20 +0530

    CORS fix

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const connectDb = require("./config/dbConnection");
 const {errorHandler} = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
+const cors = require('cors')
+
 connectDb();
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/contacts",require("./routes/contactRoutes"));
